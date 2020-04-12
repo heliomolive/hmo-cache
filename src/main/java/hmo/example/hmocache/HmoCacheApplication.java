cc09ade940626a78c342b6f84c775b9cf5198874
Helio Marci de Oliveira <heliomolive@hemac.local>
2020-04-12 19:25:18 -0300

    First commit

---
+++ src/main/java/hmo/example/hmocache/HmoCacheApplication.java
@@ -0,0 +1,13 @@
+package hmo.example.hmocache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmoCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmoCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hmo/example/hmocache/HmoCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package hmo.example.hmocache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmoCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
